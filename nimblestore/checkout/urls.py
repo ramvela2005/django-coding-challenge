ac17eab416edfb3e19b4d32bdae7a50b78f5540f
Juan Mora <juan@nimble.gt>
2024-01-25 14:32:26 -0600

    Update serializers, URLs and views

--- nimblestore/checkout/urls.py
+++ nimblestore/checkout/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("/api", views.IndexView.as_view(), name="index"),
+    path("api", views.IndexView.as_view(), name="index"),
+    path("api/products/", views.ProductListView.as_view({'get': 'list'}), name="products"),
+    path("api/order/", views.OrderView.as_view(), name="order"),
 ]
--- nimblestore/checkout/views.py
+++ nimblestore/checkout/views.py
@@ -1,7 +1,31 @@
 from django.views import generic
-
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
 
+class ProductListView(viewsets.ModelViewSet):
+    """
+    API endpoint that allows products to be listed.
+    """
+
+    # TODO Your view code here
+
+
+class OrderView(APIView):
+    parser_classes = (JSONParser,)
+
+    def post(self, request):
+        """
+        API endpoint that calculates the order total.
+        """
+        print(request.data)
+        
+        response_obj = {}
+
+        # TODO Calculate the order total here
+
+        return JsonResponse(response_obj, safe=False)
